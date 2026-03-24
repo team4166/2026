2e7f46cbc93ed458489e42762c1fe1eb1c3b3188
Mike Anderson <27mikeanderson@moraschools.org>
2026-03-24 16:36:28 -0500

    made a whole utilities branch/command

---
+++ src/main/java/frc/robot/Utilities.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+
+public class Utilities {
+  public static long millisecondsToNanoseconds(long milliseconds) {
+    return milliseconds * 1000000;
+  }
+
+  public static long millisecondsToNanoseconds(double seconds) {
+    return millisecondsToNanoseconds((long) (seconds * 1000));
+  }
+}
